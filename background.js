b5ce87822e003f0d564be65f4dd8ccd667b39b6d
sunDalik <dali1999@mail.ru>
2019-02-13 22:01:01 +0300

    small refactor

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
-var chance = 0.001;
+var chance = 0.001; //0.1%
 var deployChigurhs = `var chance = 0;
             chrome.extension.onMessage.addListener(request => {
                 chance = request.data;
                 let images = document.getElementsByTagName("img");
                 for (var i = 0; i < images.length; i++) {
                     if (Math.random() < chance) {
-                        images[i].src = chrome.extension.getURL("chigurh.jpg");
+                        images[i].src = chrome.extension.getURL("images/chigurh.jpg");
                     }
                 }
             })`;
